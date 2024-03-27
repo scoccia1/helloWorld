5fd2f4f5d2d4c9a0c93fb9eb93c67c774ee9966e
scoccia1 <scoccia@terpmail.umd.edu>
2024-03-26 20:10:26 -0400

    Created contact and favorite course html pages and created a base template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,23 @@ def about():
 @app.route('/about-css')
 def about_css():
     return render_template('about-css.html')
+
+@app.route('/base')
+def base():
+    return render_template('base.html')
+@app.route('/favorite-course')
+def favorite_course():
+    print(f'Subject entered: {request.args.get('subject')}')
+    print(f'Course number entered: {request.args.get('course_number')}')
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=["GET","POST"])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run()
