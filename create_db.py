18571e52f5a4e856c8539b18d99b3e64ab73f210
scoccia1 <scoccia@terpmail.umd.edu>
2024-04-09 17:47:32 -0400

    Modified app.py, create_db.py, models.py to accomodate users and user capabilites
    Modified student_entry.html and student_view_all.html to reflect permissions offered to specific users
    
    Added html files 404, access_denied, authorize, and error to establish authorization and page errors

--- create_db.py
+++ create_db.py
@@ -1,13 +1,13 @@
 from app import app, db
-from models import Student, Major
-import datetime as dt
+from models import Major, User
+from werkzeug.security import generate_password_hash
 
 with app.app_context():
     db.drop_all()
     db.create_all()
 
     # Initial loading of majors
-    majors = ['Accounting', 'Finance', 'Information Systems', 'International Business', 'Management', \
+    majors = ['Accounting', 'Finance', 'Information Systems', 'International Business', 'Management',
               'Operations Management & Business Analytics', 'Supply Chain Management']
     for each_major in majors:
         print(each_major)
@@ -15,18 +15,22 @@ with app.app_context():
         db.session.add(a_major)
         db.session.commit()
 
-    # Initial loading of students first_name, last_name, major_id, birth_date, is_honors. Add email to previous entries
-    students = [
-        {'student_id': '1', 'first_name': 'Robert', 'last_name':'Smith', 'student_email':'rsmith@umd.edu', 'major_id':3,
-            'birth_date': dt.datetime(2007, 6, 1), 'is_honors':1},
-        {'student_id': '2', 'first_name': 'Leo', 'last_name': 'Van Munching', 'student_email':'lvanmunch@umd.edu', 'major_id':6,
-         'birth_date': dt.datetime(2008, 3, 24), 'is_honors': 0},
+    # Initial loading of Users information
+    # Create user and password for scoccia
+    users = [
+        {'username': 'student', 'email': 'student@umd.edu', 'first_name': 'Imma', 'last_name': 'Student',
+         'password': generate_password_hash('studentpw', method='pbkdf2:sha256'), 'role': 'STUDENT'},
+        {'username': 'manager', 'email': 'manager@umd.edu', 'first_name': 'Joe', 'last_name': 'King',
+         'password': generate_password_hash('managerpw', method='pbkdf2:sha256'), 'role': 'MANAGER'},
+        {'username': 'admin', 'email': 'admin@umd.edu', 'first_name': 'Crystal', 'last_name': 'Ball',
+         'password': generate_password_hash('adminpw', method='pbkdf2:sha256'), 'role': 'ADMIN'},
+        {'username': 'scoccia', 'email': 'scoccia@terpmail.umd.edu', 'first_name': 'Sami', 'last_name': 'Coccia',
+         'password': generate_password_hash('scoccia', method='pbkdf2:sha256'), 'role': 'STUDENT'}
     ]
-## Return email for each student in the database
-    for each_student in students:
-        print(f'{each_student["first_name"]} {each_student["last_name"]} inserted into Student')
-        a_student = Student(first_name=each_student["first_name"], last_name=each_student["last_name"], student_email=each_student["student_email"],
-                            major_id=each_student["major_id"], birth_date=each_student["birth_date"],
-                            is_honors=each_student["is_honors"])
-        db.session.add(a_student)
+
+    for each_user in users:
+        print(f'{each_user["username"]} inserted into user')
+        a_user = User (username=each_user["username"], email=each_user["email"], first_name=each_user["first_name"],
+                      last_name=each_user["last_name"], password=each_user["password"], role=each_user["role"])
+        db.session.add(a_user)
         db.session.commit()
